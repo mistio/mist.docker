033aab57c800cdf647086c7e9e08ec82ae9a24da
commixon <commixon@gmail.com>
2015-04-08 16:07:39 +0300

    Fix collectd-docker hostname

--- mist-collectd/collectd-docker.sh
+++ mist-collectd/collectd-docker.sh
@@ -11,9 +11,9 @@
 #
 
 # Location of the cgroup mount point, adjust for your system
-CGROUP_MOUNT="/mnt/host"
+CGROUP_MOUNT="/sys/fs/cgroup"
 
-HOSTNAME="${COLLECTD_HOSTNAME:-localhost}"
+HOSTNAME=`cat /opt/mistio-collectd/uuid`
 INTERVAL="${COLLECTD_INTERVAL:-5}"
 
 collect ()
@@ -21,24 +21,24 @@ collect ()
   cd "$1"
 
   # If the directory length is 64, it's likely a docker instance
+  DOCKER_ID=`echo $d | sed -e 's/docker-//g' -e 's/.scope//g'`;
   LENGTH=$(expr length $1);
-  if [ "$LENGTH" -eq "64" ]; then
+  if [ "$LENGTH" -eq "77" ]; then
 
     # Shorten the name to 12 for brevity, like docker does
-    NAME=$(expr substr $1 1 12);
+    NAME=$(expr substr $DOCKER_ID 1 12);
 
     # If we are in a cpuacct cgroup, we can collect cpu usage stats
     if [ -e cpuacct.stat ]; then
         USER=$(cat cpuacct.stat | grep '^user' | awk '{ print $2; }');
         SYSTEM=$(cat cpuacct.stat | grep '^system' | awk '{ print $2; }');
         echo "PUTVAL \"$HOSTNAME/docker-$NAME/cpu-user\" interval=$INTERVAL N:$USER"
-        echo "PUTVAL \"$HOSTNAME/docker-$NAME/cpu-system\" interval=$INTERVAL N:$SYSTEM"
     fi;
 
     # If we are in a memory cgroup, we can collect memory usage stats
     if [ -e memory.stat ]; then
-        CACHE=$(cat memory.stat | grep '^cache' | awk '{ print $2; }');
-        RSS=$(cat memory.stat | grep '^rss' | awk '{ print $2; }');
+CACHE=$(cat memory.stat | grep '^cache' | awk '{ print $2; }');
+        RSS=$(cat memory.stat | grep -w rss | awk '{ print $2; }');
         echo "PUTVAL \"$HOSTNAME/docker-$NAME/memory-cached\" interval=$INTERVAL N:$CACHE"
         echo "PUTVAL \"$HOSTNAME/docker-$NAME/memory-used\" interval=$INTERVAL N:$RSS"
     fi;
@@ -46,7 +46,7 @@ collect ()
   fi;
 
   # Iterate over all sub directories
-  for d in *
+  for d in docker*.scope
   do
     if [ -d "$d" ]; then
       ( collect "$d" )
@@ -56,9 +56,8 @@ collect ()
 
 while sleep "$INTERVAL"; do
   # Collect stats on memory usage
-  ( collect "$CGROUP_MOUNT/memory" )
+  ( collect "$CGROUP_MOUNT/memory/system.slice" )
 
   # Collect stats on cpu usage
-  ( collect "$CGROUP_MOUNT/cpuacct" )
+  ( collect "$CGROUP_MOUNT/cpuacct/system.slice" )
 done
-
