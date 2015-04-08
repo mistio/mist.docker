e689fd9091d262e3973b01a42f0ba71450183eb1
commixon <commixon@gmail.com>
2015-04-08 20:22:09 +0300

    Add Monitor server as env

--- mist-collectd/entrypoint.sh
+++ mist-collectd/entrypoint.sh
@@ -3,6 +3,7 @@
 sed -i "s/USERNAME/$COLLECTD_USERNAME/g" /opt/mistio-collectd/uuid
 sed -i "s/USERNAME/$COLLECTD_USERNAME/g" /opt/mistio-collectd/collectd.conf
 sed -i "s/PASSWORD/$COLLECTD_PASSWORD/g" /opt/mistio-collectd/collectd.conf
+sed -i "s/MONITOR_SERVER/$MONITOR_SERVER/g" /opt/mistio-collectd/collectd.conf
 
 /opt/mistio-collectd/collectd.sh start
 
