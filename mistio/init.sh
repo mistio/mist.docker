a9ee05ca39cb1cc1c3e8eae728bc4951413102ca
commixon <commixon@gmail.com>
2016-01-05 13:54:02 +0200

    Update mistio image from ubuntu to debian

--- mistio/init.sh
+++ mistio/init.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-chown mist /
-chown -R mist /home/mist/mist.io
-
 if [ -z $BRANCH ]; then
     BRANCH=master
 fi
 
-exec sudo -u mist /bin/bash - << eof
 
 cd /home/mist/mist.io
 git checkout $BRANCH
@@ -16,11 +12,9 @@ git pull
 cd src/libcloud
 git pull
 cd -
-/usr/lib/erlang/erts-5.10.4/bin/epmd -daemon
 sed -i s/"frontend www localhost:8000"/"frontend www 0.0.0.0:8000"/g haproxy.conf
 sed -i s/localhost/127.0.0.1/g haproxy.conf
 
 ./bin/supervisord
 
 tail -f var/log/*
-eof
