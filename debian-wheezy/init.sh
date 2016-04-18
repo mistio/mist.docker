df3b697223b0bae6a21af84cf2c664b54555545b
Dimitris Rozakis <dimrozakis@gmail.com>
2016-04-18 19:52:39 +0300

    Clean up ubuntu & debian images, preinstall collectd

--- debian-wheezy/init.sh
+++ debian-wheezy/init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo $PUBLIC_KEY >> /root/.ssh/authorized_keys
+if [ -n "$PUBLIC_KEY" ] && ! grep $PUBLIC_KEY /root/.ssh/authorized_keys; then
+    echo $PUBLIC_KEY >> /root/.ssh/authorized_keys
+fi
+
 if [ -x /opt/mistio-collectd/collectd.sh ]; then
     /opt/mistio-collectd/collectd.sh start
 fi
+
 /usr/sbin/sshd -D
--- ubuntu-14.04/init.sh
+++ ubuntu-14.04/init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo $PUBLIC_KEY >> /root/.ssh/authorized_keys
+if [ -n "$PUBLIC_KEY" ] && ! grep $PUBLIC_KEY /root/.ssh/authorized_keys; then
+    echo $PUBLIC_KEY >> /root/.ssh/authorized_keys
+fi
+
 if [ -x /opt/mistio-collectd/collectd.sh ]; then
     /opt/mistio-collectd/collectd.sh start
 fi
